b50282ad951d6aa8696041f49c9c874a0c7215ca
lganic <55366941+lganic@users.noreply.github.com>
2026-08-19 09:38:59 -0400

    Added text content

--- pyquicktui/content.py
+++ pyquicktui/content.py
@@ -1,23 +1,41 @@
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 from .animations import overflow_text_animation
 
 if TYPE_CHECKING:
     from .window import Window
 
+@dataclass
+class RenderSpace:
+    x: int
+    y: int
+    width: int
+    height: int
+
+    def valid(self):
+        return self.width > 0 and self.height > 0
+
 class Content:
 
-    def render(self, target_window: "Window", frame_count: int):
+    def render(self, target_window: "Window", frame_count: int, space: RenderSpace) -> RenderSpace:
 
         raise NotImplementedError("Render method not implemented in inherited component.")
 
 class TextContent(Content):
 
-    def __init__(self, x: int, y: int, width: int):
+    def __init__(self, text: str):
 
-        self.x = x
-        self.y = y
-        self.width = width
+        self.text = text
 
-    def render(self, window: "Window", frame_count: int):
+    def render(self, target_window: "Window", frame_count: int, space: RenderSpace) -> RenderSpace:
 
-        
+        animated_text = overflow_text_animation(self.text, frame_count, space.width)
+
+        target_window.put_text(space.x, space.y, animated_text)
+
+        return RenderSpace(
+            space.x,
+            space.y,
+            len(animated_text),
+            1
+        )
